411a5a3814a1345c29946d3deb452767a32456f5
Pravdomir Kostov <pravdomir1998@gmail.com>
2024-04-19 23:22:32 +0300

    enable CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:4200',
+    methods: 'GET,POST,PUT,DELETE',
+    allowedHeaders: 'Content-Type,Authorization',
+  });
   await app.listen(3000);
 }
 bootstrap();
