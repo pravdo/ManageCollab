e926f64bb1543cacc454ed1005636eaa2182d008
Pravdomir Kostov <pravdomir1998@gmail.com>
2024-04-24 16:07:52 +0300

    add logout button

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -6,7 +6,7 @@ import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: '', component: HomeComponent, canActivate: [AuthGuard] },
   {
     path: 'all',
     component: AllUsersComponent,
--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
+  constructor(private router: Router) {}
 
+  logout() {
+    // Clear local storage or specific items like 'currentUser' or 'authToken'
+    localStorage.clear(); // Or localStorage.removeItem('authToken');
+
+    // Navigate to the login page after logout
+    this.router.navigate(['/login']);
+  }
 }
