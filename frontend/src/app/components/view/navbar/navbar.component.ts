49459595f226bb342a434ed08399d7b3f8087ee4
Pravdomir Kostov <pravdomir1998@gmail.com>
2024-05-02 02:17:28 +0300

    style icon dropdown

--- frontend/src/app/components/view/navbar/navbar.component.ts
+++ frontend/src/app/components/view/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, HostListener } from '@angular/core';
+import { Router } from '@angular/router';
+
 import { AuthService } from '../../../services/auth.service';
 
 @Component({
@@ -9,7 +11,32 @@ import { AuthService } from '../../../services/auth.service';
   styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent {
-  constructor(public authService: AuthService) {}
+  isDropdownOpen = false;
+
+  constructor(
+    public authService: AuthService,
+    private router: Router,
+    private elRef: ElementRef
+  ) {}
+
+  toggleDropdown(event: MouseEvent): void {
+    event.stopPropagation();
+    this.isDropdownOpen = !this.isDropdownOpen;
+  }
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent): void {
+    // Check if the click target is outside the element
+    if (!this.elRef.nativeElement.contains(event.target)) {
+      this.isDropdownOpen = false;
+    }
+  }
+
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+    this.isDropdownOpen = false; // Close dropdown upon logout
+  }
 
   getInitials(): string {
     const user = this.authService.currentUserValue;
